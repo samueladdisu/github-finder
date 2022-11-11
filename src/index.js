0ec4e37f872f716351db32ea5a18f73d9016b578
samuel addisu <samueladdisu7@gmail.com>
2022-11-11 18:45:50 +0300

    deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+// ⛔️ ReactDOM.render is no longer supported in React 18.
+// Use createRoot instead. Until you switch to the new API,
+// your app will behave as if it's running React 17.
+ReactDOM.render( // 👈️ deprecated starting React 18
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
